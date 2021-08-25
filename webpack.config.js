e8772689c9231d991318e9c4232ae9b7db2dd465
Derek Kaplan <thederekkaplan@protonmail.com>
2021-08-25 16:52:22 -0400

    Barebones initial website

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Derek Kaplan',
+      template: 'src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'index.[contenthash].css'
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader",
+        ]
+      }
+    ]
+  },
+  output: {
+    filename: 'index.[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
